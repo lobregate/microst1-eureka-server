9cf745d8a1aa876ee4d00e0c08a502553f29e880
André Lobregate <lobregate@gmail.com>
2024-05-31 18:00:29 -0300

    Add files via upload

---
+++ eureka-server/src/test/java/com/fiapst1/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiapst1.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
